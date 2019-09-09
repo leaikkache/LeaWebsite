79d26c401805f6d316507404925402e5c15f29bb
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-09-09 21:39:42 +0200

    attempt at a hexagones' timeline

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import context and provider
 import { MyContext } from '../Context';
+import '../stylesheets/Projects.css'
 
 export default function Projects() {
   return (
@@ -12,19 +13,33 @@ export default function Projects() {
               if (context.state.language === 'en') {
                 return (
                   <div>
-                    Projects
+                    <h1>Projects</h1>
                   </div>
                 ) 
               } else {
                 return (
                   <div>
-                    Projets
+                    <h1>Projets</h1>
                   </div>
                 )
               }
             })()
           }
-        </div>
+
+          {/* consider the hexagons as several lines, adjust the lines and not the hexagons themselves */}
+
+          <div className="hexagons-container">
+            <div className="hexagon"></div>
+            <div className="hexagon"></div>
+            <div className="hexagon"></div>
+            <div className="hexagon"></div>
+            <div className="hexagon"></div>
+            <div className="hexagon"></div>
+            <div className="hexagon"></div>
+          </div>
+          
+        </div>        
+
       )}
     </MyContext.Consumer>
   )
