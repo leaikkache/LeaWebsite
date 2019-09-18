982179fb18ddb27d2ab95d32392914298055db09
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-09-18 20:11:57 +0200

    attempt at homepage + navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,119 +4,134 @@ import {withRouter} from 'react-router';
 import '../stylesheets/Nav.css';
 
 class Nav extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+      french: true,
+    }
+
+    this.toggleOpen = this.toggleOpen.bind(this);
+    this.toggleLanguage = this.toggleLanguage.bind(this);
+  }
+
+  toggleOpen() {
+    this.setState({open: !this.state.open});
+  }
+
+  toggleLanguage() {
+    this.setState({french: !this.state.french})
+  }
+
   render() {
+
     return (
-      <nav className="Nav">
-        <ul className="nav-list">
-          <li>
-            <Link className="link" to="/">
+      <nav>
+        <div className="nav-languages">
+          <img onClick={this.props.context.changeLanguage} data-language="fr"src="images/fr.png" alt="french language icon"/> 
+          <img onClick={this.props.context.changeLanguage} data-language="en" src="images/en.png" alt="english language icon"/>
+        </div>
+        <div className="logo">
+          <Link className="link" to="/">
+            <img src="images/logo.png" alt=""/>
+          </Link>
+        </div>
+        <div></div>
+        <div onClick={this.toggleOpen} className={this.state.open ? 'circular-menu top-right open' : 'circular-menu top-right'}>         
+          <Link className="link disc l0 rotate" to="/partenaires">
+            {
+              (() => {
+                if (this.props.context.state.language === 'en') {
+                  return <div>Partners</div>
+                } else {
+                  return <div>Partenaires</div>
+                }
+              })()
+            }
+          </Link>
+          <Link className="link disc l1 rotate" to="/prestations">
+            {
+              (() => {
+                if (this.props.context.state.language === 'en') {
+                  return <div>Prices</div>
+                } else {
+                  return <div>Prestations</div>
+                }
+              })()
+            }
+          </Link>
+          <Link className="link disc l2 rotate" to="/lettres">
+            {
+              (() => {
+                if (this.props.context.state.language === 'en') {
+                  return <div>Letters</div>
+                } else {
+                  return <div>Lettres</div>
+                }
+              })()
+            }
+          </Link>
+          <Link className="link disc l3 rotate" to="/résumés">
+            {
+              (() => {
+                if (this.props.context.state.language === 'en') {
+                  return <div>Summaries</div>
+                } else {
+                  return <div>Résumés</div>
+                }
+              })()
+            }
+          </Link>
+          <Link className="link disc l3 rotate" to="/manifeste">
+            {
+              (() => {
+                if (this.props.context.state.language === 'en') {
+                  return <div>Manifesto</div>
+                } else {
+                  return <div>Manifeste</div>
+                }
+              })()
+            }
+          </Link>
+          <Link className="link disc l4 rotate" to="/projets">
+            {
+              (() => {
+                if (this.props.context.state.language === 'en') {
+                  return <div>Bio/Projects</div>
+                } else {
+                  return <div>Bio/Projets</div>
+                }
+              })()
+            }
+          </Link>
+          {
+            this.state.open?
+            <Link className="link disc l5 toggle">
               {
                 (() => {
                   if (this.props.context.state.language === 'en') {
-                    return "Home"
+                    return <div>Close</div>
                   } else {
-                    return "Menu"
+                    return <div>Fermer</div>
                   }
                 })()
               }
             </Link>
-          </li>
-          <li>
-            <Link className="link" to="/projets">
+            :
+            <Link className="link disc l5 toggle">
               {
                 (() => {
                   if (this.props.context.state.language === 'en') {
-                    return "Projects"
+                    return <div>Menu</div>
                   } else {
-                    return "Projets"
+                    return <div>Menu</div>
                   }
                 })()
               }
             </Link>
-          </li>
-          <li>
-            <Link className="link" to="/manifeste">
-              {
-                (() => {
-                  if (this.props.context.state.language === 'en') {
-                    return "Manifesto"
-                  } else {
-                    return "Manifeste"
-                  }
-                })()
-              }
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/résumés">
-              {
-                (() => {
-                  if (this.props.context.state.language === 'en') {
-                    return "Summaries"
-                  } else {
-                    return "Résumés"
-                  }
-                })()
-              }
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/lettres">
-              {
-                (() => {
-                  if (this.props.context.state.language === 'en') {
-                    return "Letters"
-                  } else {
-                    return "Lettres"
-                  }
-                })()
-              }
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/prestations">
-              {
-                (() => {
-                  if (this.props.context.state.language === 'en') {
-                    return "Prices"
-                  } else {
-                    return "Prestations"
-                  }
-                })()
-              }
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/partenaires">
-              {
-                (() => {
-                  if (this.props.context.state.language === 'en') {
-                    return "Partners"
-                  } else {
-                    return "Partenaires"
-                  }
-                })()
-              }
-            </Link>
-          </li>
-          <li className="nav-languages">
-            <span
-              onClick={this.props.context.changeLanguage}
-              data-language="en"
-            >
-              EN
-            </span>
-
-            <span>/</span>
-
-            <span
-              onClick={this.props.context.changeLanguage}
-              data-language="fr"
-            >
-              FR
-            </span>
-          </li>
-        </ul>
+          }
+        </div>
       </nav>
     )
   }
